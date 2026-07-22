3ddcf6ae7922d0537eebe468c1248fa52800102d
p00her <pooh@pooh.spb.ru>
2026-07-22 13:59:13 +0300

    Barcode: height 35

--- src/components/Barcode.tsx
+++ src/components/Barcode.tsx
@@ -8,7 +8,7 @@ interface Props {
   height?: number;
 }
 
-export default function Barcode({ value, width = 2.4, height = 40 }: Props) {
+export default function Barcode({ value, width = 2.4, height = 35 }: Props) {
   const ref = useRef<SVGSVGElement>(null);
   const theme = useTheme();
   const color = theme.palette.mode === 'dark' ? '#fff' : '#000';
